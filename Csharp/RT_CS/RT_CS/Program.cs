84b0bd0bd8cafaa5e2183664d725016f795df28a
Cyberpunked007 <94081345+Cyberpunked007@users.noreply.github.com>
2021-11-11 16:56:32 -0800

    Added all the classes discussed so far

--- Csharp/RT_CS/RT_CS/Program.cs
+++ Csharp/RT_CS/RT_CS/Program.cs
@@ -2,6 +2,43 @@
 
 namespace RT_CS
 {
+    namespace utility
+    {
+        struct Vector3
+        {
+            public float x, y, z;
+        }
+
+        namespace SceneObjects
+        {
+            class Scene
+            {
+                private float length, width;
+            }
+        }
+
+        namespace shapes
+        {
+            class Shape
+            { }
+
+            class Sphere : Shape
+            {
+                private Vector3 centre;
+            }
+
+            class Triangle : Shape 
+            {
+                protected Vector3 VertexA, VertexB, VertexC;
+            }
+            class Rectangle : Shape
+            {
+                private Vector3 VertexD;
+            }
+        }
+    }
+
+   
     class Program
     {
         static void Main(string[] args)
