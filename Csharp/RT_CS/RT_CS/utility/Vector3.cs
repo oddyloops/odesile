8e9bc4032a61c046221de2d44db2e31f1c636204
oddyloops <odesileadedayo@gmail.com>
2021-11-12 15:50:36 -0800

    Added operators for division, magnitude and normalization

--- Csharp/RT_CS/RT_CS/utility/Vector3.cs
+++ Csharp/RT_CS/RT_CS/utility/Vector3.cs
@@ -7,5 +7,26 @@ namespace RT_CS.utility
     public struct Vector3
     {
         public float x, y, z;
+
+        public float Magnitude()
+        {
+            return (float)Math.Sqrt(x * x + y * y + z * z);
+        }
+
+        public Vector3 Normalize()
+        {
+            float magnitude = Magnitude();
+            return this / magnitude;
+        }
+
+        public static Vector3 operator /(Vector3 v, float f)
+        {
+            return new Vector3()
+            {
+                x = v.x / f,
+                y = v.y / f,
+                z = v.z / f
+            };
+        }
     }
 }
