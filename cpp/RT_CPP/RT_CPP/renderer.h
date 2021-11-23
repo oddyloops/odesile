2caeca7f730069effc4ed3f438e966291ed48052
Cyberpunked007 <94081345+Cyberpunked007@users.noreply.github.com>
2021-11-22 17:31:36 -0800

    cpp corrections

--- cpp/RT_CPP/RT_CPP/renderer.h
+++ cpp/RT_CPP/RT_CPP/renderer.h
@@ -6,18 +6,18 @@
 using namespace scene_objects;
 using namespace std;
 
-namespace utilities
+namespace utility
 {
 	class renderer
 	{
 	private:
 		scene_database& db;
 		
-		intersection_record& run_intersection(camera const& cam,vector<shape*> const& shapes, int pX, int pY);
+		intersection_record* run_intersection(camera const& cam,vector<shape*> const& shapes, int pX, int pY);
 
 	public:
 		renderer(scene_database& db);
 
-		void render();
+		vector3** render();
 	};
 }
