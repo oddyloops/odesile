a3eb761c6254927c190b980d1259fd83c48ec182
Cyberpunked007 <94081345+Cyberpunked007@users.noreply.github.com>
2021-11-23 18:13:35 -0800

    added the intersection to the rectangle object

--- Csharp/RT_CS/RT_CS/shapes/Triangle.cs
+++ Csharp/RT_CS/RT_CS/shapes/Triangle.cs
@@ -9,11 +9,36 @@ namespace RT_CS.shapes
 
     public class Triangle : Shape
     {
-        protected Vector3 vertexA, vertexB, vertexC;
+        protected Vector3 vertexA, vertexB, vertexC, normal;
+        protected float D;
 
-        public Triangle(int id)  :base(id)
+        public Triangle(int id, Vector3 v1, Vector3 v2, Vector3 v3)  :base(id)
         {
-
+            vertexA = v1;
+            vertexB = v2;
+            vertexC = v3;
+            normal = ((v3 - v1) * (v2 - v1)).Normalize();
+            D = v1.Magnitude();
+        }
+        public IntersectionRecord RayPlaneIntersection(Ray r)
+        {
+            IntersectionRecord inter = new IntersectionRecord();
+            float nordir = Vector3.Dot(r.direct, normal);
+            if (Math.Abs(nordir) < float.Epsilon)
+            {
+                return inter;
+            }
+            else
+            {
+                float d = (-(Vector3.Dot(r.posit, normal) + D))/nordir;
+                if (d > 0)
+                {
+                    inter.Distance = d;
+                    inter.Hit = true;
+                    inter.Point = r.posit + d * r.direct;
+                }
+                return inter;
+            }
         }
 
         public override void Intersect(Ray r, IntersectionRecord intersection)
